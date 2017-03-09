aa9a2754a39dbe5e56eafebc935f2b91c217ef8e
fbatke <felix-batke@web.de>
2017-03-09 15:50:17 +0100

    added repo functions

--- src/main/java/com/crap/sms/domain/model/Subscriber.java
+++ src/main/java/com/crap/sms/domain/model/Subscriber.java
@@ -11,4 +11,68 @@ public class Subscriber {
     private Subscription subscription;
     private String foreName;
     private String surName;
+    private Integer freeMinutes;
+    private Integer dataVolume;
+
+    public Subscriber(BigInteger IMSI, Terminal terminal, Subscription subscription, String foreName, String surName, Integer freeMinutes, Integer dataVolume) {
+        this.IMSI = IMSI;
+        this.terminal = terminal;
+        this.subscription = subscription;
+        this.foreName = foreName;
+        this.surName = surName;
+        this.freeMinutes = freeMinutes;
+        this.dataVolume = dataVolume;
+    }
+
+    public BigInteger getIMSI() {
+        return IMSI;
+    }
+
+    public Terminal getTerminal() {
+        return terminal;
+    }
+
+    public void setTerminal(Terminal terminal) {
+        this.terminal = terminal;
+    }
+
+    public Subscription getSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription(Subscription subscription) {
+        this.subscription = subscription;
+    }
+
+    public String getForeName() {
+        return foreName;
+    }
+
+    public void setForeName(String foreName) {
+        this.foreName = foreName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public Integer getFreeMinutes() {
+        return freeMinutes;
+    }
+
+    public void setFreeMinutes(Integer freeMinutes) {
+        this.freeMinutes = freeMinutes;
+    }
+
+    public Integer getDataVolume() {
+        return dataVolume;
+    }
+
+    public void setDataVolume(Integer dataVolume) {
+        this.dataVolume = dataVolume;
+    }
 }
