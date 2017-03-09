565a5ef582fc2dbbb97f25cc6f5dd2a8a645938e
Martin Geßenich <martin.gessenich@rwth-aachen.de>
2017-03-09 15:45:09 +0100

    service UserManagement

---
+++ src/main/java/com/crap/sms/service/UserManagement.java
@@ -0,0 +1,17 @@
+package com.crap.sms.service;
+
+import com.crap.sms.domain.model.User;
+
+/**
+ * Created by Martin Geßenich on 09.03.2017.
+ */
+public class UserManagement {
+
+    public User createUser(String name , String password){
+        return null;
+    }
+
+    public User findUser( String name ) {
+        return null;
+    }
+}
