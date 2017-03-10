68acf73a8a2d1c3d93d62e12c00f787b18f248a5
Martin Geßenich <martin.gessenich@rwth-aachen.de>
2017-03-10 17:12:23 +0100

    Session stuff

--- src/main/java/com/crap/sms/domain/model/Session.java
+++ src/main/java/com/crap/sms/domain/model/Session.java
@@ -3,4 +3,27 @@ package com.crap.sms.domain.model;
 import java.io.Serializable;
 
 public class Session implements Serializable {
+    private int seconds;
+    private RAN connection;
+    private double dataRate;
+
+    /**
+     * Only phone connection
+     * @param seconds
+     */
+    public Session(int seconds) {
+        this.seconds = seconds;
+        this.connection = null;
+    }
+
+    /**
+     * Data connection
+     * @param seconds
+     * @param connection
+     */
+    public Session(int seconds, RAN connection, double dataRate) {
+        this.seconds = seconds;
+        this.connection = connection;
+        this.dataRate = dataRate;
+    }
 }
---
+++ src/main/java/com/crap/sms/service/RANService.java
@@ -0,0 +1,12 @@
+package com.crap.sms.service;
+
+import com.crap.sms.domain.model.RAN;
+
+/**
+ * Created by Martin Geßenich on 10.03.2017.
+ */
+public class RANService {
+    public static RAN getRandomRAN() {
+        Math.random()*
+    }
+}
