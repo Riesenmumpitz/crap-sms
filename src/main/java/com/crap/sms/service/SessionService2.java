0b5bf19662ec7ae07cc89d65a2e627d94f420ee3
caroline-leh <caroline.lehmann@ericsson.com>
2017-03-19 23:16:56 +0100

    SessionService2 file added

---
+++ src/main/java/com/crap/sms/service/SessionService2.java
@@ -0,0 +1,100 @@
+package com.crap.sms.service;
+
+import com.crap.sms.domain.model.RAN;
+import com.crap.sms.domain.model.Session;
+import com.crap.sms.domain.model.Subscriber;
+import com.crap.sms.domain.model.Subscription;
+import com.crap.sms.domain.repository.SubscriptionRepository;
+
+public class SessionService2 {
+
+	public Session createPhoneSession(int seconds) {
+		if (!(seconds >= 0 && seconds < Integer.MAX_VALUE)) {
+			return null;
+		}
+		return new Session(seconds);
+	}
+
+	public Session createDataSession(int seconds, RAN connection) {
+		if (!(seconds >= 0 && seconds < Integer.MAX_VALUE)
+				|| connection == null) {
+			return null;
+		}
+		return new Session(seconds, connection, generateRandomConnectionRate());
+	}
+
+	private double generateRandomConnectionRate() {
+		switch ((int) Math.random() * 4) {
+		case 0:
+			return 0;
+		case 1:
+			return .1;
+		case 2:
+			return .25;
+		case 3:
+			return .5;
+		default:
+			return 42;
+		}
+
+	}
+
+	public boolean bookSession(Subscriber sub, Session session) {
+		String print = "";
+		boolean usedUp = false;
+		int usedSec = 0;
+		// add subscriber info
+		print += "\n" + sub.toString() + "\n";
+
+		if (session.getConnection() == null) {
+			// voice call service
+
+			sub.setUsedMinutes(sub.getUsedMinutes() + session.getSeconds());
+			print += "Service: Voice Call\n";
+			usedSec += session.getSeconds();
+		} else {
+			// data service
+
+			print += "Service: Data service\n";
+			int achievedDataRate;
+			int usedVolume = 0;
+			RAN connection = session.getConnection();
+			Subscription s = SubscriptionRepository.getInstance()
+					.getByUniqueName(sub.getSubscription());
+
+			// calculate achieved data rate
+			if (connection.equals("G4")) {
+				achievedDataRate = (int) (300 * session.getDataRate());
+			} else if (connection.equals("G3")) {
+				achievedDataRate = (int) (20 * session.getDataRate());
+			} else {
+				return false;
+			}
+
+			// use data volume as long as requested and possible
+			while (usedSec <= session.getSeconds()) {
+				//check if data volume is used up
+				if (s.getDataVolume() - sub.getDataVolume() <= 0
+						|| s.getDataVolume()
+								- (sub.getDataVolume() + achievedDataRate) <= 0) {
+					usedUp = true;
+					break;
+				}
+				usedSec += 1;
+				usedVolume += achievedDataRate;
+				//update total used data volume of subscriber
+				sub.setDataVolume(sub.getDataVolume() + achievedDataRate);
+			}
+
+			print += "Achieved data-rate: " + achievedDataRate + " Mbit/s\n";
+			print += "Used data volume: " + usedVolume + "\n";
+
+		}
+		print += "Time used: " + usedSec + "\n";
+		if (usedUp) {
+			print += "DATA VOLUME is USED UP\n";
+		}
+		System.out.println(print);
+		return true;
+	}
+}
