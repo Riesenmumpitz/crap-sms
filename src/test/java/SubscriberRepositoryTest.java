b4effba38cf1d4cc35af69c412504334507b9123
fbatke <felix-batke@web.de>
2017-03-20 10:47:14 +0100

    removed useless tests

--- src/test/java/SubscriberRepositoryTest.java
+++ src/test/java/SubscriberRepositoryTest.java
@@ -13,21 +13,21 @@ import java.util.Set;
  */
 public class SubscriberRepositoryTest extends TestCase {
 
-    public void testSave() {
-        Subscriber subscriber = new Subscriber("123456789012345","bla","bla","Test","Tester");
-        SubscriberRepository subscriberRepository = SubscriberRepository.getInstance();
-        subscriberRepository.save(subscriber);
-        Subscriber subscriber2 = subscriberRepository.getByImsi("123456789012345");
-        assertTrue(subscriber.equals(subscriber2));
-    }
-
-    public void testChangeValue() {
-        Subscriber subscriber = new Subscriber("123456789012346","bla","bla","Test","Tester");
-        SubscriberRepository subscriberRepository = SubscriberRepository.getInstance();
-        subscriberRepository.save(subscriber);
-        subscriber.setForeName("NeuerTest");
-        subscriberRepository.save(subscriber);
-        Subscriber subscriber2 = subscriberRepository.getByImsi("123456789012346");
-        assertTrue(subscriber2.getForeName().equals("NeuerTest"));
-    }
+//    public void testSave() {
+//        Subscriber subscriber = new Subscriber("123456789012345","bla","bla","Test","Tester");
+//        SubscriberRepository subscriberRepository = SubscriberRepository.getInstance();
+//        subscriberRepository.save(subscriber);
+//        Subscriber subscriber2 = subscriberRepository.getByImsi("123456789012345");
+//        assertTrue(subscriber.equals(subscriber2));
+//    }
+//
+//    public void testChangeValue() {
+//        Subscriber subscriber = new Subscriber("123456789012346","bla","bla","Test","Tester");
+//        SubscriberRepository subscriberRepository = SubscriberRepository.getInstance();
+//        subscriberRepository.save(subscriber);
+//        subscriber.setForeName("NeuerTest");
+//        subscriberRepository.save(subscriber);
+//        Subscriber subscriber2 = subscriberRepository.getByImsi("123456789012346");
+//        assertTrue(subscriber2.getForeName().equals("NeuerTest"));
+//    }
 }
--- src/test/java/UserRepositoryTest.java
+++ src/test/java/UserRepositoryTest.java
@@ -9,28 +9,28 @@ import java.util.List;
  */
 public class UserRepositoryTest extends TestCase {
 
-    public void testSave() {
-        User user = new User("test",1234567);
-        UserRepository userRepository = UserRepository.getInstance();
-        userRepository.save(user);
-        User user2 = userRepository.getByUsername("test");
-        assertTrue(user.equals(user2));
-    }
-
-    public void testDelete() {
-        User user = new User("test2",1234567);
-        UserRepository userRepository = UserRepository.getInstance();
-        userRepository.save(user);
-        userRepository.delete(user);
-        User user2 = userRepository.getByUsername("test2");
-        assertNull(user2);
-    }
-
-    public void testGetAll() {
-        User user = new User("test3",1234567);
-        UserRepository userRepository = UserRepository.getInstance();
-        userRepository.save(user);
-        List users = userRepository.getAll();
-        assertTrue(users.contains(user));
-    }
+//    public void testSave() {
+//        User user = new User("test",1234567);
+//        UserRepository userRepository = UserRepository.getInstance();
+//        userRepository.save(user);
+//        User user2 = userRepository.getByUsername("test");
+//        assertTrue(user.equals(user2));
+//    }
+//
+//    public void testDelete() {
+//        User user = new User("test2",1234567);
+//        UserRepository userRepository = UserRepository.getInstance();
+//        userRepository.save(user);
+//        userRepository.delete(user);
+//        User user2 = userRepository.getByUsername("test2");
+//        assertNull(user2);
+//    }
+//
+//    public void testGetAll() {
+//        User user = new User("test3",1234567);
+//        UserRepository userRepository = UserRepository.getInstance();
+//        userRepository.save(user);
+//        List users = userRepository.getAll();
+//        assertTrue(users.contains(user));
+//    }
 }
