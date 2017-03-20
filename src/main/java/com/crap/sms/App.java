d66606b6aac0c558d135662e5870dfeb55993267
CG <CG@apcg01.team4.de>
2017-03-20 09:16:55 +0100

    "Hi, I will manage your sessions" entfernt

--- src/main/java/com/crap/sms/App.java
+++ src/main/java/com/crap/sms/App.java
@@ -3,15 +3,12 @@ package com.crap.sms;
 import com.crap.sms.controller.Login;
 import com.crap.sms.controller.UI;
 
-public class App
-{
+public class App {
 
-    public static void main( String[] args )
-    {
-    	if (Login.login()) {
-    		UI ui = new UI();
-    		ui.work();
-    	}
-    	System.out.println("Hi, I will manage your sessions!");
-    }
+	public static void main(String[] args) {
+		if (Login.login()) {
+			UI ui = new UI();
+			ui.work();
+		}
+	}
 }
